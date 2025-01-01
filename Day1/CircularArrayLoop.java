4de62ad1b212342c608ca805c93f5d5c776ffb86
sandeepjoshi22 <37525158+sandeepjoshi22@users.noreply.github.com>
2025-01-01 16:50:36 +0000

    Setup for Day 1

---
+++ Day1/CircularArrayLoop.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class CircularArrayLoop {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Circular loop");
+
+        // ifLoopExists: https://www.educative.io/courses/grokking-coding-interview/circular-array-loop
+    }
+    
+}
---
+++ Day1/FindNextPalindrome.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class FindNextPalindrome {
+    public static void main(String[] args) {
+        System.out.println("Hello World from next palindrome");
+
+        // findNextPalindrome: https://www.educative.io/courses/grokking-coding-interview/next-palindrome-using-same-digits
+    }
+    
+}
