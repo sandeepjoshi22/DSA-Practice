11711c7c69551309d0562fa07d8e81e2e6251e43
sandeepjoshi22 <37525158+sandeepjoshi22@users.noreply.github.com>
2025-01-03 12:56:25 +0000

    Day 2: PalindromeList question started

---
+++ Day2/PalindromeList.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class PalindromeList {
+    public static void main(String[] args) {
+
+        //https://www.educative.io/courses/grokking-coding-interview/palindrome-linked-list
+        
+    }
+
+    public static boolean palindrome(LinkedListNode head) {
+        // Replace this placeholder return statement with your code
+        return false;
+    }
+}
